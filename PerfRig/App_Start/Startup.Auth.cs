59047452cb63aed752d06768c53641042b23e0fc
Dustin Iser <diser@confluence.com>
2015-04-27 13:01:21 -0400

    Remove IdentityConfig and the AccountController.

--- PerfRig/App_Start/Startup.Auth.cs
+++ PerfRig/App_Start/Startup.Auth.cs
@@ -8,7 +8,6 @@ using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Google;
 using Microsoft.Owin.Security.OAuth;
 using Owin;
-using PerfRig.Providers;
 using PerfRig.Models;
 
 namespace PerfRig
@@ -22,10 +21,6 @@ namespace PerfRig
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
-            // Configure the db context and user manager to use a single instance per request
-            app.CreatePerOwinContext(ApplicationDbContext.Create);
-            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
-
             // Enable the application to use a cookie to store information for the signed in user
             // and to use a cookie to temporarily store information about a user logging in with a third party login provider
             app.UseCookieAuthentication(new CookieAuthenticationOptions());
@@ -36,7 +31,6 @@ namespace PerfRig
             OAuthOptions = new OAuthAuthorizationServerOptions
             {
                 TokenEndpointPath = new PathString("/Token"),
-                Provider = new ApplicationOAuthProvider(PublicClientId),
                 AuthorizeEndpointPath = new PathString("/api/Account/ExternalLogin"),
                 AccessTokenExpireTimeSpan = TimeSpan.FromDays(14),
                 AllowInsecureHttp = true
