56e83c1779c2351dcfb762abc0891b6b252f4cda
Dustin Iser <diser@confluence.com>
2015-06-02 20:28:17 -0400

    Move graph displaying function to its own file.

---
+++ PerfRig/Scripts/displaygraph.js
@@ -0,0 +1,38 @@
+﻿function displayGraph(timeUri) {
+    var container = document.getElementById('visualization');
+
+    $(document).ready(function () {
+        $.getJSON(timeUri)
+            .done(function (data) {
+                var items = [];
+                $.each(data, function (key, item) {
+                    items.push({ id: item.Id, x: item.TimeStamp, y: item.ElapsedTime });
+                })
+
+                var dataset = new vis.DataSet(items);
+
+                function compare(a, b) {
+                    if (a.TimeStamp < b.TimeStamp)
+                        return -1;
+                    if (a.TimeStamp > b.TimeStamp)
+                        return 1;
+                    return 0;
+                }
+
+                items.sort(compare);
+                var options = {
+                    style: 'bar',
+                    barChart: { width: 50, align: 'center', handleOverlap: 'sideBySide' }, // align: left, center, right
+                    drawPoints: false,
+                    dataAxis: {
+                        icons: true
+                    },
+                    orientation: 'top',
+                    start: items[0].TimeStamp,
+                    end: items[items.length - 1].TimeStamp
+                };
+
+                var graph2d = new vis.Graph2d(container, items, options);
+            })
+    })
+}
